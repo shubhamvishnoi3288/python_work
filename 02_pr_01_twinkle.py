daa46e285b3ac19f6dc37dd0a2d10758237dc6c0
SHUBHAM KUMAR VISHNOI <65655892+shubhamvishnoi3288@users.noreply.github.com>
2021-01-05 08:10:30 -0800

    Add files via upload

---
+++ 02_pr_01_twinkle.py
@@ -0,0 +1,7 @@
+
+# This is also a comment just like the above line
+
+print('''Twinkle, twinkle, little star,
+How I wonder what you are!
+Up above the world so high,
+Like a diamond in the sky.''')
