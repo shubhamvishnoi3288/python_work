c3f08fec53fd5e81e590a6a1566369a399db3d8a
SHUBHAM KUMAR VISHNOI <65655892+shubhamvishnoi3288@users.noreply.github.com>
2021-01-06 05:03:19 -0800

    Add files via upload

---
+++ 01_variable.py
@@ -0,0 +1,19 @@
+a_122 = '''my name is shubham'''
+# a = 'my name is famous'
+# a = "my work is coding"
+b = 345
+c = 45.32
+d = True
+# d = None
+
+# Printing the variables
+print(a)
+print(b)
+print(c)
+print(d)
+
+# Printing the type of variables
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
