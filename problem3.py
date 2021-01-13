dd6e9f0083361e9a4d05255e72c750658e72ebbe
SHUBHAM KUMAR VISHNOI <65655892+shubhamvishnoi3288@users.noreply.github.com>
2021-01-13 07:28:09 -0800

    Add files via upload

---
+++ problem3.py
@@ -0,0 +1,12 @@
+#s ={18, "18", 18.2}
+#print(s)
+#print(len(s))
+
+#s ={20, 20.0, "20"}
+#print(s)
+#print(len(s))
+
+
+s ={}     # yaha par log hamesha galti karte reason why.. kyunki yaha par ye ek empty set lagta hai but set nahi hota ek dictionary hoti hai... that solve's 
+#type(s)
+print(type(s))
