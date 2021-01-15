24631fbcae9d4e2b7f934cfb2c33b9bd984cc702
SHUBHAM KUMAR VISHNOI <65655892+shubhamvishnoi3288@users.noreply.github.com>
2021-01-15 02:57:52 -0800

    Add files via upload

---
+++ string.py
@@ -0,0 +1,22 @@
+#greeting = "Good Morning,"
+name ="Shubham"
+#print(type(name))
+#print(greeting + name)      # concatination two string
+#c = greeting + name
+#print(c)
+#print(name[0])
+#print(name[1])
+#print(name[2])
+#print(name[3]) 
+#print(name[4])
+#print(name[5])
+#print(name[6])
+#   name[3] ="d"   = #-->    does not work
+#print(name[0:7]
+#print(name[0:])
+#c = name[-4:-1]
+#print(c)
+name ="shubhamisgood"
+#d = name[1:4:1]
+d= name[0::3]
+print(d)
