1122348e42556ec3802f8a2cc5a5c605ee5a3612
SHUBHAM KUMAR VISHNOI <65655892+shubhamvishnoi3288@users.noreply.github.com>
2021-01-16 01:41:58 -0800

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,13 @@
+# creating a tuble using ()
+#t = (1, 2, 3, 4, 5)      # this is the tuple multiple element in a tuple.... 
+t1 = ()     # this is a empty tuple...
+print(t1)
+#t2 =(1)       # ntuple with single element(wrong way) kyunki ye tuple nahi hai sirf elemet hai ab
+t2 =(1,)      # tuple with single element (right way)
+print(t2)
+#printing the element of a tuple
+#print(t[0])
+#print(t[1])
+
+# cannot update the values of a tuple value..
+#t[0] = 34  
