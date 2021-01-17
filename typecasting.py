623745668f17fdc745a21c7d7fe6be1602d12e05
SHUBHAM KUMAR VISHNOI <65655892+shubhamvishnoi3288@users.noreply.github.com>
2021-01-17 05:57:15 -0800

    Add files via upload

---
+++ typecasting.py
@@ -0,0 +1,7 @@
+# typecasting is the way to convert  one datatype to another datatype.....
+#a = "3534"
+a = "3534.23"
+#a = int(a)         # this is a type-casting
+a = float(a)
+print(type(a)) 
+print(a + 5)
