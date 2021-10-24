afafcfb3cd1f8ffa27c93ba42a51df95435031bd
SHUBHAM KUMAR VISHNOI <65655892+shubhamvishnoi3288@users.noreply.github.com>
2021-10-24 17:55:01 +0530

    Update sum_list.py

--- sum_list.py
+++ sum_list.py
@@ -1,7 +1,7 @@
 #a = (4,6,2,3)
 #print(a)
 #print(4+6+2+3)
-#print(a[0]+a[1]+a[2]+a[3])
+#print(a[0]+a[1]+a[2]+a[3])                 
 #print(sum(a))     # this is the best way \
 a =(3,45,54,4,4,4,4,4,4,4,4,4,455,5,5,6,6,5,455,64,654,44,4)
 c= a.count(4)
